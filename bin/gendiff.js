58288f88c98a051e5ad8b1a6f1e0c0e1eabd7d42
Ilya Sokolov <ilyasokolov@192.168.1.8>
2022-10-09 04:26:37 +0300

    add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ import { Command } from 'commander';
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 program.parse();
