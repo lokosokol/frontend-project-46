5fa5b77c4270099a51022e235e63cce6a396b600
Ilya Sokolov <ilyasokolov@192.168.1.3>
2022-11-23 05:18:16 +0300

    test

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 
 const genDiff = (filepath1, filepath2) => {
-  const obj1 = JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), filepath1)),
-  );
-  const obj2 = JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), filepath2)),
-  );
+  const obj1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath1)));
+  const obj2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath2)));
   const obj1Keys = Object.keys(obj1);
   const obj2Keys = Object.keys(obj2);
   const keys = Object.keys({ ...obj1, ...obj2 }).sort();
@@ -17,11 +12,8 @@ const genDiff = (filepath1, filepath2) => {
       return obj1[key] === obj2[key]
         ? [...acc, `    ${key}: ${obj1[key]}`]
         : [...acc, `  - ${key}: ${obj1[key]}`, `  + ${key}: ${obj2[key]}`];
-    } else {
-      return obj1Keys.includes(key)
-        ? [...acc, `  - ${key}: ${obj1[key]}`]
-        : [...acc, `  + ${key}: ${obj2[key]}`];
     }
+    return obj1Keys.includes(key) ? [...acc, `  - ${key}: ${obj1[key]}`] : [...acc, `  + ${key}: ${obj2[key]}`];
   };
   const result = keys.reduce(checkIfAdded, []);
   return `{\n${result.join('\n')}\n}`;
